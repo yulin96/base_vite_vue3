3a83cc15be14dc147fae77f2d764c0792fc53f4d
Yulin Zhao <illqq@icloud.com>
2023-08-19 17:31:49 +0800

    🌈 style:

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -4,15 +4,8 @@ const linkTo = inject('linkTo') as ILinkTo
 
 <template>
   <div class="Index">
-    <div class="content" @click="showLoadingToast({ message: '123123', forbidClick: false })">123</div>
+    <div class="content"></div>
   </div>
 </template>
 
-<style scoped lang="scss">
-.Index {
-  .content {
-    height: 6100px;
-    background: url("https://oss.eventnet.cn/H5/CHENGHAO/LEGION818/images/bg02.jpg") top 400px center/100% auto no-repeat;
-  }
-}
-</style>
+<style scoped lang="scss"></style>
