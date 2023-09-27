6a9fd49392828b76ca56f84be87ebb1294e99cd2
Yulin <aaayulin@gmail.com>
2023-09-27 10:49:48 +0800

    ✨ feat:

--- src/utils/wxShare.ts
+++ src/utils/wxShare.ts
@@ -17,24 +17,23 @@ export const registerWxShare = () => {
     WxShare(shareContent)
 
   dd.ready(function () {
-    if (dd.env.platform !== 'notInDingTalk')
-      dd.biz.navigation.setRight({
-        show: true, //控制按钮显示， true 显示， false 隐藏， 默认true
-        control: true, //是否控制点击事件，true 控制，false 不控制， 默认false
-        text: '···', //控制显示文本，空字符串表示显示默认文本
-        onSuccess: function (result: any) {
-          //如果control为true，则onSuccess将在发生按钮点击事件被回调
-          dd.biz.util.share({
-            type: 0, //分享类型，0:全部组件 默认； 1:只能分享到钉钉；2:不能分享，只有刷新按钮
-            url: shareContent.link,
-            content: shareContent.desc,
-            title: shareContent.title,
-            image: shareContent.imgUrl,
-            onSuccess: function () {},
-            onFail: function () {},
-          })
-        },
-        onFail: function () {},
-      })
+    dd.biz.navigation.setRight({
+      show: true, //控制按钮显示， true 显示， false 隐藏， 默认true
+      control: true, //是否控制点击事件，true 控制，false 不控制， 默认false
+      text: '···', //控制显示文本，空字符串表示显示默认文本
+      onSuccess: function (result: any) {
+        //如果control为true，则onSuccess将在发生按钮点击事件被回调
+        dd.biz.util.share({
+          type: 0, //分享类型，0:全部组件 默认； 1:只能分享到钉钉；2:不能分享，只有刷新按钮
+          url: shareContent.link,
+          content: shareContent.desc,
+          title: shareContent.title,
+          image: shareContent.imgUrl,
+          onSuccess: function () {},
+          onFail: function () {},
+        })
+      },
+      onFail: function () {},
+    })
   })
 }
