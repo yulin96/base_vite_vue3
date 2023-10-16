f2067e2982ab96f0f61a1a8066ea849d59df79fc
Yulin <aaayulin@gmail.com>
2023-10-16 12:06:22 +0800

    ✨ feat:

--- src/utils/clickLottie.ts
+++ src/utils/clickLottie.ts
@@ -1,4 +1,4 @@
-fetch('https://oss.eventnet.cn/H5/zz/public/lotties/btn/btn1.json')
+fetch('https://oss.eventnet.cn/H5/zz/public/lotties/btn/btn3.json')
   .then((res) => res.json())
   .then((res) => {
     window['loadingLottieJson'] = res
@@ -6,28 +6,29 @@ fetch('https://oss.eventnet.cn/H5/zz/public/lotties/btn/btn1.json')
 
 export const showLottie = (e: MouseEvent) => {
   if (!window['loadingLottieJson']) return
+  const width = 200
 
-  const { clientX, clientY, pointerId } = e as PointerEvent
-
+  const { clientX, clientY } = e
   const div = document.createElement('div')
   div.style.position = 'fixed'
-  div.style.width = '300px'
-  div.style.height = '300px'
-  div.style.top = `${clientY - 150}px`
-  div.style.left = `${clientX - 150}px`
+  div.style.width = `${width}px`
+  div.style.height = `${width}px`
+  div.style.top = `${clientY - width / 2}px`
+  div.style.left = `${clientX - width / 2}px`
   div.style.pointerEvents = 'none'
+  div.style.transform = `rotate(${Math.floor(Math.random() * 180)}deg)`
   document.body.appendChild(div)
 
-  const tempid = `tempid${pointerId}`
-  window[tempid] = lottie.loadAnimation({
+  const templo = lottie.loadAnimation({
     container: div,
-    renderer: 'svg',
     loop: false,
     autoplay: true,
     animationData: window['loadingLottieJson'],
+    renderer: 'canvas',
   })
-  window[tempid].addEventListener('complete', () => {
+  templo.setSpeed(1.6)
+
+  templo.addEventListener('complete', () => {
     document.body.removeChild(div)
-    delete window[tempid]
   })
 }
--- src/views/index.vue
+++ src/views/index.vue
@@ -1,7 +1,11 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const next = (e: any) => {
+  showLottie(e)
+}
+</script>
 
 <template>
   <div class="index">
-    <div class="content"></div>
+    <div class="content" @click="next"></div>
   </div>
 </template>
