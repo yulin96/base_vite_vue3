7d83a251b681ec63ee8a61f7a80d78a301dc8ca4
Yulin <aaayulin@gmail.com>
2023-11-10 11:16:37 +0800

    ✨ feat:

---
+++ src/utils/convert.ts
@@ -0,0 +1,20 @@
+export const jpgUrlToPngBlob = (url: string) => {
+  return new Promise<Blob>((resolve, reject) => {
+    const image = new Image()
+    image.crossOrigin = 'anonymous'
+    image.onload = (img) => {
+      const target = img.target as HTMLImageElement
+      const canvas = document.createElement('canvas')
+      canvas.width = target.width
+      canvas.height = target.height
+      const context = canvas.getContext('2d')
+      context?.drawImage(target, 0, 0)
+      canvas.toBlob((blob) => {
+        if (blob) resolve(blob)
+        else reject()
+      }, 'image/png')
+    }
+    image.onerror = reject
+    image.src = url
+  })
+}
