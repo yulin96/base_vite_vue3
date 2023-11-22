87e074ab459e14e8e57aa9192ea125f9468e98a9
Yulin <aaayulin@gmail.com>
2023-11-22 17:36:45 +0800

    Update route transition animation in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,12 @@
 <script setup lang="ts">
 /* 路由动画 */
-const { transitionName } = useRouteTransition('Zoom')
+const { transitionName } = useRouteTransition('Slide')
 /* 分享 */
 registerWxShare()
 </script>
 
 <template>
-  <router-view class="wrapper" v-slot="{ Component, route }">
+  <router-view class="wrapper" v-slot="{ Component }">
     <transition :name="transitionName">
       <keep-alive exclude="">
         <component :is="Component" />
