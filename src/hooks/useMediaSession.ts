3f530e8d0244f56e6e272b0ecc862e49ca21c474
Yulin <aaayulin@gmail.com>
2023-11-23 10:03:57 +0800

    Refactor useMediaSession function to accept MediaMetadata interface

--- src/hooks/useMediaSession.ts
+++ src/hooks/useMediaSession.ts
@@ -1,13 +1,6 @@
 import { onMounted, onUnmounted } from 'vue'
 
-interface IMetaData {
-  title?: string
-  artist?: string
-  album?: string
-  artwork?: Array<{ src: string }>
-}
-
-export const useMediaSession = (ele: Ref<HTMLMediaElement | null>, option?: IMetaData) => {
+export const useMediaSession = (ele: Ref<HTMLMediaElement | null>, option?: MediaMetadata) => {
   onMounted(() => {
     if (!(ele.value instanceof HTMLMediaElement)) return null
     if ('mediaSession' in navigator) {
@@ -15,11 +8,13 @@ export const useMediaSession = (ele: Ref<HTMLMediaElement | null>, option?: IMet
         title: option?.title || import.meta.env.VITE_APP_SHARE_TITLE || import.meta.env.VITE_APP_TITLE,
         artist: option?.artist || import.meta.env.VITE_APP_SHARE_DESC,
         album: option?.album || import.meta.env.VITE_APP_SHARE_TITLE || import.meta.env.VITE_APP_TITLE,
-        artwork: option?.artwork || [
-          {
-            src: import.meta.env.VITE_APP_SHARE_IMGURL || 'https://oss.eventnet.cn/H5/zz/public/favicon.png',
-          },
-        ],
+        artwork: option?.artwork
+          ? [...option.artwork]
+          : [
+              {
+                src: import.meta.env.VITE_APP_SHARE_IMGURL || 'https://oss.eventnet.cn/H5/zz/public/favicon.png',
+              },
+            ],
       })
       navigator.mediaSession.setActionHandler('play', function () {
         ele.value?.play()
