dcd2018f23d9e9a050833201eb7e24f089bec4c8
Yulin <aaayulin@gmail.com>
2023-11-24 10:59:43 +0800

    Commented out unused code and removed unnecessary styles

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,18 @@
 import type { Config } from 'tailwindcss'
 import plugin from 'tailwindcss/plugin'
 
-const convertUnit = (length = 100, name = 'px') =>
-  Object.fromEntries(Array.from({ length: length + 1 }, (_, i) => [i, `${i}${name}`]))
+// const convertUnit = (length = 100, name = 'px') =>
+//   Object.fromEntries(Array.from({ length: length + 1 }, (_, i) => [i, `${i}${name}`]))
 
 export default {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}', './scan/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    spacing: convertUnit(100),
+    // spacing: convertUnit(100),
     extend: {
-      width: convertUnit(1000),
-      height: convertUnit(1000),
-      fontSize: convertUnit(100),
-      borderRadius: convertUnit(100),
+      // width: convertUnit(1000),
+      // height: convertUnit(1000),
+      // fontSize: convertUnit(100),
+      // borderRadius: convertUnit(100),
       colors: {},
       animation: {
         'spin-slow': 'spin 4s linear infinite',
@@ -33,7 +33,7 @@ export default {
         },
       })
       addComponents({
-        '.flex-center': {
+        '.center': {
           'display': 'flex',
           'justify-content': 'center',
           'align-items': 'center',
