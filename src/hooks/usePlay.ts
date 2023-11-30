b696ff2ad8e7a5f217754466a8517fa321dcc077
Yulin <aaayulin@gmail.com>
2023-11-30 14:42:59 +0800

    Refactor usePlay hook to accept an optional boolean parameter

--- src/hooks/usePlay.ts
+++ src/hooks/usePlay.ts
@@ -1,11 +1,11 @@
 import { ref } from 'vue'
 
-export const usePlay = () => {
-  const play = ref(false)
+export const usePlay = (paly?: boolean) => {
+  const play = ref(paly ?? false)
 
   const togglePlay = (toggle?: boolean) => {
     play.value = toggle ?? true
   }
 
-  return [play, togglePlay]
+  return [play, togglePlay] as [typeof play, typeof togglePlay]
 }
