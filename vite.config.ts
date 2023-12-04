27470d2842a3c7ab13c564a5faed1b87eb92aa31
Yulin <aaayulin@gmail.com>
2023-12-04 17:25:41 +0800

    Add @vitejs/plugin-vue-jsx to package.json and vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from 'node:path'
 import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import legacy from '@vitejs/plugin-legacy'
+import vueJsx from '@vitejs/plugin-vue-jsx'
 import { webUpdateNotice } from '@plugin-web-update-notification/vite'
 
 import AutoImport from 'unplugin-auto-import/vite'
@@ -24,6 +25,7 @@ export default defineConfig(({ command }) => ({
   plugins: [
     command === 'build' ? handleCheck() : undefined,
     vue({ script: { defineModel: true } }),
+    vueJsx(),
     webUpdateNotice({
       versionType: 'build_timestamp',
       hiddenDefaultNotification: true,
