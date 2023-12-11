1952640c45b6972b6d8a287260b76afe30204220
Yulin <aaayulin@gmail.com>
2023-12-11 16:30:10 +0800

    Add keyframes for bounce animation

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -17,6 +17,12 @@ export default {
       animation: {
         'spin-slow': 'spin 4s linear infinite',
       },
+      keyframes: {
+        bounce: {
+          '0%, 100%': { 'transform': 'none', 'animation-timing-function': 'cubic-bezier(0,0,0.2,1)' },
+          '50%': { 'transform': 'translateY(-25%)', 'animation-timing-function': 'cubic-bezier(0.8,0,1,1)' },
+        },
+      },
     },
   },
   plugins: [
