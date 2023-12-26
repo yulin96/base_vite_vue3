16340127c6596463e612bed0361c02ac609374e1
Yulin <aaayulin@gmail.com>
2023-12-26 16:50:39 +0800

    Add decryption functionality for OSS access credentials

---
+++ src/utils/crypto.ts
@@ -0,0 +1,19 @@
+import CryptoJS from 'crypto-js'
+
+export const encrypt = (text: string, keyStr: string, ivStr: string) => {
+  const key = CryptoJS.enc.Utf8.parse(keyStr)
+  const iv = CryptoJS.enc.Utf8.parse(ivStr)
+  const encrypted = CryptoJS.AES.encrypt(text, key, { iv }).toString()
+  return encrypted
+}
+
+export const decrypt = (text: string, keyStr: string, ivStr: string) => {
+  const key = CryptoJS.enc.Utf8.parse(keyStr)
+  const iv = CryptoJS.enc.Utf8.parse(ivStr)
+  try {
+    const decrypted = JSON.parse(CryptoJS.AES.decrypt(text, key, { iv }).toString(CryptoJS.enc.Utf8))
+    return decrypted
+  } catch (error) {
+    return null
+  }
+}
