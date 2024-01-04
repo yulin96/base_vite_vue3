c5280c770c49eb9eff9b28accba8ac1831cfd4f3
Yulin <aaayulin@gmail.com>
2024-01-04 16:06:03 +0800

    🚀 feat:typing move to ts

--- src/lang/index.ts
+++ src/lang/index.ts
@@ -4,16 +4,20 @@ import { createI18n } from 'vue-i18n'
 
 export type MessageSchema = typeof zhCN
 
+declare module 'vue-i18n' {
+  export interface DefineLocaleMessage extends MessageSchema {}
+}
+
 const { VITE_APP_LOCALSTORAGE_NAME: localName } = import.meta.env
 const localeName = useLocalStorage((localName || 'test') + '-local', 'zh-CN')
 
-const i18n = createI18n<false>({
+const i18n = createI18n<[MessageSchema], 'zh-CN' | 'en'>({
   inheritLocale: true,
   legacy: false,
   locale: localeName.value,
   messages: {
-    'en': en,
     'zh-CN': zhCN,
+    'en': en,
   },
 })
 
--- typings/i18n.d.ts
+++
@@ -1,6 +0,0 @@
-import type { MessageSchema } from '~/lang'
-import { DefineLocaleMessage } from 'vue-i18n'
-
-declare module 'vue-i18n' {
-  export interface DefineLocaleMessage extends MessageSchema {}
-}
