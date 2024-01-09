5574b4ce4e4e3b01e5e8acdafde80d041c260761
Yulin <aaayulin@gmail.com>
2024-01-09 09:13:16 +0800

    🐞 fix: 修复滑页bug，修复useScroll arrivedState在ios下可能存在的bug

--- src/hooks/useSlide.ts
+++ src/hooks/useSlide.ts
@@ -9,62 +9,49 @@ interface ISlideOptions {
   slideNumber?: number
 }
 
-/**
- * A custom hook for handling slide functionality.
- * @param {Object} options - The options for the slide functionality.
- * @param {string} options.eleName - The name of the element to apply the slide functionality to.
- * @param {Function} [options.prev] - The function to be called when the previous slide action is triggered.
- * @param {Function} [options.next] - The function to be called when the next slide action is triggered.
- * @param {Function} [options.prevScroll] - The function to be called when scrolling to the previous slide.
- * @param {Function} [options.nextScroll] - The function to be called when scrolling to the next slide.
- * @param {number} [options.slideNumber=100] - The threshold number for triggering the slide action.
- */
-export const useSlide = ({ eleName, prev, next, prevScroll, nextScroll, slideNumber = 100 }: ISlideOptions): void => {
-  const startMove = ref({ clientY: 0, once: true })
+export const useSlide = ({ eleName, prev, next, prevScroll, nextScroll, slideNumber = 100 }: ISlideOptions) => {
+  const startMove = ref({ pageY: 0, once: true })
 
   onMounted(() => {
     const ele = document.querySelector(eleName) as HTMLDivElement | null
     if (!ele) return console.error('ele is not found')
-    const { arrivedState, isScrolling } = useScroll(ele, { offset: { bottom: 0 } })
+    const { arrivedState } = useScroll(ele, { offset: { bottom: 0 } })
 
     let lock = false
-    ele.addEventListener('touchstart', () => {
+    ele.addEventListener('touchstart', (t) => {
       lock = false
     })
 
     ele.addEventListener('touchmove', (t) => {
-      const clientY = t.changedTouches[0].clientY
-      if (isScrolling.value) startMove.value.clientY = clientY
+      const pageY = t.changedTouches[0].pageY
 
-      if (arrivedState.top || arrivedState.bottom) {
+      if (arrivedState.top || arrivedState.bottom || ele.scrollTop < 0) {
         if (startMove.value.once) {
-          startMove.value.clientY = clientY
+          startMove.value.pageY = pageY
           startMove.value.once = false
         }
 
-        const startClientY = startMove.value.clientY
-
-        if (Math.abs(startClientY - clientY) > slideNumber) {
+        if (Math.abs(startMove.value.pageY - pageY) > slideNumber) {
+          if (lock) return
+          lock = true
           if (t.cancelable) {
             t.preventDefault()
           }
-          if (lock) return
-          lock = true
-          return startClientY > clientY ? next?.() : prev?.()
+          return startMove.value.pageY > pageY ? next?.() : prev?.()
         }
 
-        if (Math.abs(startClientY - clientY) < 20) return
+        if (Math.abs(startMove.value.pageY - pageY) < 20) return
 
-        if (startClientY > clientY) {
-          return nextScroll?.(Math.round(startClientY - clientY))
-        } else if (startClientY < clientY) {
-          return prevScroll?.(Math.round(startClientY - clientY))
+        if (startMove.value.pageY > pageY) {
+          return nextScroll?.(Math.round(startMove.value.pageY - pageY))
+        } else if (startMove.value.pageY < pageY) {
+          return prevScroll?.(Math.round(startMove.value.pageY - pageY))
         }
       }
     })
 
-    ele.addEventListener('touchend', () => {
-      startMove.value.clientY = 0
+    ele.addEventListener('touchend', (e) => {
+      startMove.value.pageY = 0
       startMove.value.once = true
       lock = false
     })
