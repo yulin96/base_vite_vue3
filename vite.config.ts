0a6834f26c51bde2de0f3665a80db2ade9c17a00
Yulin <aaayulin@gmail.com>
2024-02-16 19:56:41 +0800

    移除不必要的代码

--- vite.config.ts
+++ vite.config.ts
@@ -27,9 +27,6 @@ if (env.VITE_OSS_ROOT_DIRNAME && env.VITE_OSS_DIRNAME) {
 }
 
 export default defineConfig(({ command }) => ({
-  define: {
-    __VUE_PROD_DEVTOOLS__: command !== 'build',
-  },
   plugins: [
     command === 'build' ? handleCheck() : undefined,
     topLevelAwait(),
