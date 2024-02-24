e55ed512886868e47387cc0ba05415406d381ac1
Yulin <aaayulin@gmail.com>
2024-02-24 22:36:12 +0800

    优化usePreviewImage钩子函数的代码结构

--- src/hooks/usePreviewImage.ts
+++ src/hooks/usePreviewImage.ts
@@ -4,11 +4,8 @@ import { showImagePreview } from 'vant'
 
 export const usePreviewImage = () => {
   const previewImage = (urls: string[], index: number = 0) => {
-    if (isWeChat) {
-      WxPreviewImage(urls[index], urls)
-    } else {
-      showImagePreview({ images: urls, startPosition: index })
-    }
+    if (isWeChat) WxPreviewImage(urls[index], urls)
+    else showImagePreview({ images: urls, startPosition: index })
   }
   return { previewImage }
 }
