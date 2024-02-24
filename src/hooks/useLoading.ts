4dbbb2a4c04784c72fc6e82c0f045c5ee3492c32
Yulin <aaayulin@gmail.com>
2024-02-24 22:34:24 +0800

    优化useLoading和useFixLoading钩子函数的性能

--- src/hooks/useLoading.ts
+++ src/hooks/useLoading.ts
@@ -1,51 +1,56 @@
-export const useLoading = (loadingImgList: string[], next?: Function, delay: number = 300) => {
-  let sto: number | any
-  const count = ref<number>(0)
-  let progressImg = 0
-  const imgs = loadingImgList
+export const useLoading = (imgList: string[], next?: Function, delay: number = 300) => {
+  let sto: NodeJS.Timeout
+  const progressValue = ref(0)
+
+  const count = computed(() =>
+    (progressValue.value / imgList.length) * 100 >= 100 ? 100 : (progressValue.value / imgList.length) * 100,
+  )
 
   const start = () => {
-    for (const img of imgs) {
+    for (const img of imgList) {
       const image = new Image()
       image.src = img
-      const loadFun = () => {
-        progressImg++
-        count.value = (progressImg / imgs.length) * 100 >= 100 ? 100 : (progressImg / imgs.length) * 100
+      const load = () => {
+        progressValue.value++
+
         if (count.value >= 100) {
           sto = setTimeout(() => {
             next !== undefined && next()
           }, delay)
         }
       }
-      image.onload = loadFun
-      image.onerror = loadFun
+      image.onload = load
+      image.onerror = load
     }
   }
 
   onUnmounted(() => {
     clearTimeout(sto)
   })
+
   onDeactivated(() => {
     clearTimeout(sto)
   })
+
   return { count, start }
 }
 
 // 假的loading效果
 export const useFixLoading = (speed: number = 20) => {
-  const count = ref<number>(0)
+  const count = ref(0)
 
-  const Interval1 = setInterval(() => {
+  const IntervalFun = setInterval(() => {
     count.value++
-    count.value >= 100 && clearInterval(Interval1)
+    count.value >= 100 && clearInterval(IntervalFun)
   }, speed)
 
   onUnmounted(() => {
-    clearInterval(Interval1)
+    clearInterval(IntervalFun)
   })
 
   onDeactivated(() => {
-    clearInterval(Interval1)
+    clearInterval(IntervalFun)
   })
+
   return { count }
 }
