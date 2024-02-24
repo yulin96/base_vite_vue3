4beb63497424f0a9d58e0b68fc6de334121604a5
Yulin <aaayulin@gmail.com>
2024-02-24 22:10:49 +0800

    🦄 refactor:重构useUploadOSS

--- src/hooks/useUploadOSS.ts
+++ src/hooks/useUploadOSS.ts
@@ -3,28 +3,25 @@ import { toFormData } from '~/utils/tools'
 import { v1 } from 'uuid'
 import { showSuccessToast } from 'vant'
 
-/**
- *
- * @param project_uuid 项目id
- * @param file 文件
- * @param filetype 文件类型
- * @param filenameStart 文件名前缀
- * @param needLoading 是否需要loading
- */
+interface IUploadOption {
+  projectID: string
+  file: File
+  filetype?: string
+  filenameStart?: string
+  needLoading?: boolean
+}
 
-export const useUploadImage = async (
-  project_uuid: string,
-  file: File | Blob,
-  filetype?: string,
-  filenameStart: string = 'az',
-  needLoading: boolean = false,
-) => {
+export const useUploadOSS = async (option: IUploadOption) => {
+  const { projectID, file, filetype = 'png', filenameStart = 'zh', needLoading = true } = option
+
+  needLoading && showLoadingToast({ message: '上传中...' })
+
   return new Promise<string>((resolve, _) => {
     axios
-      .post('https://center-service.event1.cn/oss/sign', { project_uuid })
+      .post('https://center-service.event1.cn/oss/sign', { project_uuid: projectID })
       .then(({ data: { data: configData } }) => {
         const { host, dir, accessid: OSSAccessKeyId, policy, signature: Signature } = configData
-        const key = dir + '/' + filenameStart + v1() + '.' + (filetype ? filetype : 'png')
+        const key = `${dir}/${filenameStart}-${v1()}.${filetype}`
         axios
           .post(
             host,
@@ -40,7 +37,7 @@ export const useUploadImage = async (
           )
           .then((res) => {
             if (res.status == 200) {
-              const fileUrl = 'https://oss2.eventnet.cn/' + key
+              const fileUrl = `https://oss2.eventnet.cn/${key}`
               needLoading && showSuccessToast({ message: '上传成功' })
               resolve(fileUrl)
             } else {
