5ad683e07548b6bae1af786b20ba83a9244a0721
Yulin <aaayulin@gmail.com>
2024-02-27 22:38:39 +0800

    优化帧动画逻辑和性能

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,43 +1,12 @@
 <script setup lang="ts">
-import FrameAnimation from '~/utils/FrameAnimation'
-import { CanvasAnimation } from '~/utils/canvasAnimation'
-
 /*  */
 onMounted(() => {
   gsap.context(() => {}, '.index')
-
-  new CanvasAnimation({
-    urlPrefix: 'https://oss.eventnet.cn/H5/zz/auto/ehairudi_20240222/x/s2/',
-    suffix: '.webp',
-    MaxL: 34,
-    el: 'effect2',
-    dpr: 1,
-    poster: true,
-    posterUrl: 'https://oss.eventnet.cn/H5/zz/auto/ehairudi_20240222/x/s2/0.webp',
-    loop: true,
-    frame: 16,
-    gap: 0,
-  }).play()
-
-  new FrameAnimation({
-    el: '#effect3',
-    urlPrefix: 'https://oss.eventnet.cn/H5/zz/auto/ehairudi_20240222/x/s2/',
-    urlSuffix: '.webp',
-    maxLength: 34,
-    loop: true,
-    initIndex: 10,
-    autoPlay: true,
-
-    poster: 'https://oss.eventnet.cn/H5/zz/auto/ehairudi_20240222/x/s2/1.webp',
-  })
 })
 </script>
 
 <template>
-  <div class="index bg-slate-800">
-    <div class="content">
-      <canvas class="h-540 w-750" id="effect2"></canvas>
-      <canvas class="h-540 w-750" id="effect3"></canvas>
-    </div>
+  <div class="index">
+    <div class="content"></div>
   </div>
 </template>
--- src/utils/FrameAnimation.ts
+++ src/utils/FrameAnimation.ts
@@ -1,25 +1,34 @@
-interface FrameAnimationOption {
+export interface FrameAnimationOption {
   el: string | HTMLCanvasElement // canvas元素id或者canvas元素
   urlPrefix: string // 图片路径前缀
   urlSuffix: string // 图片路径后缀
   maxLength: number // 图片最大序号
+  fps?: number // 帧率
 
-  initIndex?: number // 初始化序号
   autoPlay?: boolean // 是否自动播放
   poster?: string // 是否需要封面图片
   loop?: boolean // 是否循环播放
-  loopStartIndex?: 1 // 循环开始序号
-  loopEndIndex?: 20 // 循环结束序号
+  loopNum?: number // 循环次数
+  loopStartIndex?: number // 循环开始序号
+  loopEndIndex?: number // 循环结束序号
 }
 
 //图片序列帧动画
 export default class FrameAnimation {
+  private option: FrameAnimationOption
   public canPlay = false
   public cycles = 0
+  public playing = false
   private animationElement: HTMLCanvasElement
   private animationCtx: CanvasRenderingContext2D
   private width: number
   private height: number
+  private posterImg!: HTMLImageElement
+  private path!: [number, number, number, number]
+
+  private imgList: HTMLImageElement[] = []
+  private currentIndex = 1
+  private fromTo: null | [number, number] = null
 
   constructor(option: FrameAnimationOption) {
     if (typeof option.el === 'string') {
@@ -32,51 +41,101 @@ export default class FrameAnimation {
     } else {
       this.animationElement = option.el
     }
+
+    this.option = option
     this.animationCtx = this.animationElement.getContext('2d')!
     this.width = this.animationElement.clientWidth * window.devicePixelRatio
     this.height = this.animationElement.clientHeight * window.devicePixelRatio
     this.animationElement.width = this.width
     this.animationElement.height = this.height
+    this.path = [0, 0, this.width, this.height]
 
-    this.initHeight()
-    this.init(option)
+    this.init()
   }
 
-  private initHeight() {}
-
-  private async init(option: FrameAnimationOption) {
-    const { maxLength, initIndex = 1, urlPrefix, urlSuffix, autoPlay = true, poster } = option
+  private async init() {
+    const {
+      maxLength,
+      urlPrefix,
+      urlSuffix,
+      autoPlay = true,
+      poster,
+      loop = true,
+      loopStartIndex = 0,
+      loopEndIndex = maxLength,
+    } = this.option
 
     if (poster) {
       const img = await loadImg(poster)
-
-      this.animationCtx.drawImage(img, 0, 0, this.width, this.height)
+      this.posterImg = img
+      this.animationCtx.drawImage(img, ...this.path)
     }
 
-    const imgLength = maxLength - initIndex + 1
-    const imgList = Array.from({ length: imgLength }, (_, i) => loadImg(`${urlPrefix}${i + initIndex}${urlSuffix}`))
+    if (loop) this.fromTo = [loopStartIndex, loopEndIndex - 1]
 
+    const imgList = Array.from({ length: maxLength }, (_, i) => loadImg(`${urlPrefix}${i + 1}${urlSuffix}`))
     let index = 0
-    for await (const _ of imgList) index++
-    if (index !== imgLength) return console.error('图片加载失败')
+    for await (const img of imgList) {
+      this.imgList.push(img)
+      index++
+    }
+    if (index !== maxLength) return console.error('图片加载失败')
     this.canPlay = true
     if (autoPlay) this.play()
   }
 
   public play() {
-    console.log('play')
-    console.log(this.animationElement)
+    if (this.playing) return console.error('已经在播放中')
+    this.playing = true
+    if (!this.fromTo) this.fromTo = [0, this.option.maxLength - 1]
+    const [from, to] = this.fromTo
+    this.currentIndex = this.currentIndex !== 0 ? this.currentIndex : this.cycles === 0 ? 0 : from
+
+    const { fps = 60 } = this.option
+    let lastTime = 0
+
+    const _fps = Math.floor(1000 / fps)
+    const frame = (time: number) => {
+      if (!this.playing) return
+      const interval =
+        this.cycles === 0 ? _fps : (_fps / (this.fromTo![1] + 1 - this.fromTo![0])) * this.option.maxLength
+      if (time - lastTime >= interval) {
+        this.animationCtx.clearRect(...this.path)
+        this.animationCtx.drawImage(this.imgList[this.currentIndex], ...this.path)
+        if (this.currentIndex < to) {
+          this.currentIndex++
+        } else {
+          this.currentIndex = from
+          if (++this.cycles >= (this.option?.loopNum || Infinity)) return
+        }
+
+        lastTime = time
+      }
+      requestAnimationFrame(frame)
+    }
+    requestAnimationFrame(frame)
   }
 
-  public pause() {}
+  public pause() {
+    this.playing = false
+  }
 
-  public stop() {}
+  public stop() {
+    this.animationCtx.clearRect(...this.path)
+    this.animationCtx.drawImage(this.posterImg, ...this.path)
+  }
+
+  get isPlay() {
+    return this.playing
+  }
 }
 
 async function loadImg(url: string) {
   return new Promise<HTMLImageElement>((resolve, reject) => {
     const img = new Image()
-    img.onload = () => resolve(img)
+    img.onload = () => {
+      resolve(img)
+    }
     img.onerror = reject
     img.src = url
   })
