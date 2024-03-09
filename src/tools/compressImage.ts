2be321250b757ef5779e45be06f63d47433fa5b8
Yulin <aaayulin@gmail.com>
2024-03-09 09:58:39 +0800

    参数可选

--- src/tools/compressImage.ts
+++ src/tools/compressImage.ts
@@ -1,6 +1,6 @@
 import Compressor from 'compressorjs'
 
-export const compressImage = (file: File, options: Compressor.Options) => {
+export const compressImage = (file: File, options?: Compressor.Options) => {
   return new Promise<File | Blob>((resolve, reject) => {
     new Compressor(file, {
       quality: 0.6,
