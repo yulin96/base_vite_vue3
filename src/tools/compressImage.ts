f93cf9517daf5fae7f0cd669b9786982360cf53c
Yulin <aaayulin@gmail.com>
2024-03-09 09:33:47 +0800

    🚀 feat:

---
+++ src/tools/compressImage.ts
@@ -0,0 +1,17 @@
+import Compressor from 'compressorjs'
+
+export const compressImage = (file: File, options: Compressor.Options) => {
+  return new Promise<File | Blob>((resolve, reject) => {
+    new Compressor(file, {
+      quality: 0.6,
+      convertTypes: ['image/jpeg'],
+      ...options,
+      success(result) {
+        resolve(result)
+      },
+      error(err) {
+        reject(err)
+      },
+    })
+  })
+}
