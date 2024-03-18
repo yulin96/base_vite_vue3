bcb61988721a40821c048819d9280d36db34f159
Yulin <aaayulin@gmail.com>
2024-03-18 13:28:44 +0800

    🚀 feat:

--- src/tools/compressImage.ts
+++ src/tools/compressImage.ts
@@ -1,13 +1,13 @@
 import Compressor from 'compressorjs'
 
 export const compressImage = (file: File, options?: Compressor.Options) => {
-  return new Promise<File | Blob>((resolve, reject) => {
+  return new Promise<File>((resolve, reject) => {
     new Compressor(file, {
       quality: 0.6,
       mimeType: 'image/jpeg',
       ...options,
       success(result) {
-        resolve(result)
+        resolve(result as File)
       },
       error(err) {
         reject(err)
