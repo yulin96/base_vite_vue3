f9fb2d7ceb877d38bdecdcf913aa08e13956ae6f
Yulin <aaayulin@gmail.com>
2024-04-07 16:57:25 +0800

    🚀 feat:

---
+++ src/components/ZWxApplet.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+const props = defineProps<{
+  username: string
+  path: string
+}>()
+</script>
+
+<template>
+  <component
+    :is="'wx-open-launch-weapp'"
+    id="launch-btn"
+    :username="props.username"
+    :path="props.path"
+    style="position: absolute; top: 0; left: 0; right: 0; bottom: 0"
+  >
+    <component :is="'script'" type="text/wxtag-template">
+      <component :is="'style'"> .btn { position: absolute; top: 0; left: 0; right: 0; bottom: 0; } </component>
+      <div class="btn"></div>
+    </component>
+  </component>
+</template>
