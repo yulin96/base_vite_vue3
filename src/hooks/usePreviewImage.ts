140bfd6ea0063e83d92743d83e98c14d3539847a
Yulin <aaayulin@gmail.com>
2024-04-29 09:56:35 +0800

    更新usePreviewImage.ts

--- src/hooks/usePreviewImage.ts
+++ src/hooks/usePreviewImage.ts
@@ -2,7 +2,9 @@ import { showImagePreview } from 'vant'
 import { wxPreviewImage } from '~/tools/wx'
 import { isWeChat } from '~/utils/uaParser'
 
-export const previewImage = (urls: string[], index: number = 0) => {
-  if (isWeChat) wxPreviewImage(urls[index], urls)
-  else showImagePreview({ images: urls, startPosition: index })
+export const showImage = (url: string[] | string, index: number = 0) => {
+  const imageUrls = Array.isArray(url) ? url : [url]
+
+  if (isWeChat && isHttps()) wxPreviewImage(imageUrls[index], imageUrls)
+  else showImagePreview({ images: imageUrls, startPosition: index })
 }
