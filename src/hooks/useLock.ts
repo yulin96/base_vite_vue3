937f9e8560d3ff7e292aa8c18791e2411e605ffe
yulin <aaayulin@gmail.com>
2024-07-03 18:07:46 +0800

    fix

--- src/hooks/useLock.ts
+++ src/hooks/useLock.ts
@@ -34,7 +34,7 @@ export const useLock = (showProgress = true, delay = 500) => {
           resolve(res)
         })
         .catch((err) => {
-          if (err.name !== 'CanceledError') showNotify({ type: 'warning', message: '正在加载中...' })
+          showNotify({ type: 'warning', message: '正在加载中...' })
           reject(err)
         })
         .finally(() => {
@@ -72,7 +72,7 @@ export const useLock = (showProgress = true, delay = 500) => {
           resolve(res)
         })
         .catch((err) => {
-          if (err.name !== 'CanceledError') showNotify({ type: 'warning', message: '正在加载中...' })
+          showNotify({ type: 'warning', message: '正在加载中...' })
           reject(err)
         })
         .finally(() => {
