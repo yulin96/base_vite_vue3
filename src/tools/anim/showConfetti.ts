ce3ddda7440ce5e56fd2d9936d17911bce4a8a9e
yulin <aaayulin@gmail.com>
2024-07-04 14:14:45 +0800

    chore: 调整庆祝效果的扩散范围

--- src/tools/anim/showConfetti.ts
+++ src/tools/anim/showConfetti.ts
@@ -3,9 +3,9 @@ import confetti from 'canvas-confetti'
 export const showConfetti = () => {
   confetti({
     particleCount: 120,
-    spread: 60,
+    spread: 70,
     angle: -90,
     origin: { y: -0.6 },
-    colors: ['#fff', '#091f86', '#091f86', '#fbe1b7', '#fdd0bd', '#f1f1f1'],
+    // colors: ['#fff', '#091f86', '#091f86', '#fbe1b7', '#fdd0bd', '#f1f1f1'],
   })
 }
