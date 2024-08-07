15c7a75d088d1661a0508fe7a2e7c41d68f9f201
yulin <aaayulin@gmail.com>
2024-08-07 18:35:14 +0800

    test

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,4 +1,11 @@
 <script setup lang="ts">
+const itemRefs = ref<HTMLDivElement[]>([])
+
+const test = () => {
+  console.log(itemRefs.value[5])
+
+  itemRefs.value[5]?.scrollIntoView({ behavior: 'smooth', inline: 'center', block: 'start' })
+}
 /*  */
 onMounted(() => {
   gsap.context(() => {
@@ -9,7 +16,22 @@ onMounted(() => {
 
 <template>
   <section class="index">
-    <main class="content"></main>
+    <main class="content pt-200">
+      <div class="flex h-100 w-full items-center space-x-20 overflow-x-auto">
+        <div
+          v-for="item in 20"
+          :key="item"
+          ref="itemRefs"
+          class="flex-shrink-0 flex-grow-0 rounded-full border px-20 py-10"
+        >
+          {{ `第${item}个元素` }}
+        </div>
+      </div>
+
+      <div class="center mt-200 h-100 w-full border" @click="test">测试</div>
+
+      <div v-for="item in 20" :key="item" class="my-60">{{ item }}</div>
+    </main>
   </section>
 </template>
 
