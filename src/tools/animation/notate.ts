16cb5604018f9705ad724200e201fbe0e8460a71
yulin <aaayulin@gmail.com>
2024-08-07 15:59:52 +0800

    feat: 添加notate函数

---
+++ src/tools/animation/notate.ts
@@ -0,0 +1,23 @@
+import { annotate } from 'rough-notation'
+import type { RoughAnnotationConfig } from 'rough-notation/lib/model'
+import { sleep } from '~/utils/common'
+
+export const notate = (name: string, config: RoughAnnotationConfig) => {
+  const ele = document.querySelector(name)
+  if (!ele) return console.error('element not found')
+
+  const _notate = annotate(document.querySelector(name)!, config)
+  return _notate
+}
+
+export async function notateAuto(name: string, config: RoughAnnotationConfig, removeTime: number = 0) {
+  const _notate = notate(name, config)
+  if (!_notate) return
+
+  _notate.show()
+
+  const animationDuration = config?.animationDuration || 800
+  await sleep(animationDuration + removeTime)
+
+  if (removeTime) _notate.remove()
+}
