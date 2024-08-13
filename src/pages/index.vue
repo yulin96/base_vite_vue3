dc15ae4d57595b813a59af991a5eeb0a9dab15c3
yulin <aaayulin@gmail.com>
2024-08-13 21:52:35 +0800

    wip

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,4 +1,19 @@
 <script setup lang="ts">
+import html2canvas from '~/tools/html2canvas'
+
+const test = () => {
+  const html2 = document.querySelector('[html2canvas]') as HTMLDivElement
+  html2canvas(html2, {
+    useCORS: true,
+    width: html2.offsetWidth,
+    height: html2.offsetHeight,
+    backgroundColor: 'none',
+    ignoreElements: (el) => el.classList.contains('html2-hide'),
+  }).then((canvas) => {
+    const url = canvas.toDataURL('image/jpeg')
+    console.log(url)
+  })
+}
 /*  */
 onMounted(() => {
   gsap.context(() => {
@@ -8,8 +23,17 @@ onMounted(() => {
 </script>
 
 <template>
-  <section class="index">
-    <main class="content"></main>
+  <section class="index" html2canvas>
+    <main class="content">
+      123123
+      <div class="h-300 w-600">
+        <common-image-scale
+          :url="'https://oss.eventnet.cn/H5/zz/auto/benz2407_20240704/assets/bg-DIEWG6gQ.jpg'"
+        ></common-image-scale>
+      </div>
+
+      <div @click="test">测试</div>
+    </main>
   </section>
 </template>
 
