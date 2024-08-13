fce8e2b43a1b0f217bfa154ae6040a7e0328e189
yulin <aaayulin@gmail.com>
2024-08-13 19:14:42 +0800

    chore: zoomist使用本地加载

--- src/components/common/image-scale.vue
+++ src/components/common/image-scale.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import Zoomist from 'zoomist'
+import Zoomist from '~/tools/zoomist'
 
 const props = withDefaults(defineProps<{ url: string; fillType?: 'cover' | 'contain' | 'none' }>(), {
   fillType: 'cover',
---
+++ src/tools/zoomist/index.ts
@@ -0,0 +1,9 @@
+import './zoomist.min.css'
+import './zoomist_min.js'
+
+/**
+ * zoomist v1 版本
+ */
+const Zoomist = window.Zoomist as unknown as typeof import('zoomist')
+
+export default Zoomist
