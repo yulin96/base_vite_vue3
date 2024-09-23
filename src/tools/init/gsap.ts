1662c57b6e0dad44322de16593f13ded71d785f7
yulin <aaayulin@gmail.com>
2024-09-23 13:56:06 +0800

    feat: 添加gsap插件注册

--- src/tools/init/gsap.ts
+++ src/tools/init/gsap.ts
@@ -1,4 +1,11 @@
 import gsap from 'gsap'
+import { MotionPathPlugin } from 'gsap/MotionPathPlugin'
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
+import { TextPlugin } from 'gsap/TextPlugin'
+
+gsap.registerPlugin(MotionPathPlugin)
+gsap.registerPlugin(TextPlugin)
+gsap.registerPlugin(ScrollTrigger)
 
 gsap.config({ force3D: true })
 gsap.defaults({
