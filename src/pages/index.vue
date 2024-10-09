6fd06702a7becb755ae3454f0e7f2ad7b52b0dbb
yulin <aaayulin@gmail.com>
2024-10-09 18:25:55 +0800

    test

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,7 +1,13 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+console.log('test 123123')
+</script>
 
 <template>
-  <div></div>
+  <div class="h-[1080px] w-[1920px]">
+    <div v-for="x in 6" :key="x" :data-x="x" class="flex h-1/6 w-full bg-red-200/20">
+      <div v-for="y in 8" :key="y" :data-y="y" class="h-full w-[12.5%] bg-sky-600/60"></div>
+    </div>
+  </div>
 </template>
 
 <route lang="json">
