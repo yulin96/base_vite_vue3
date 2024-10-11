446563a17c06646301a9b3733f18d0cef05662c8
yulin <aaayulin@gmail.com>
2024-10-11 18:36:51 +0800

    wip

---
+++ deployFtp.js
@@ -0,0 +1,29 @@
+import { Client } from 'basic-ftp'
+import { exit } from 'process'
+
+async function clientAndUpload() {
+  const client = new Client()
+  client.ftp.verbose = true
+
+  await client.access({
+    host: '8.146.209.208',
+    port: 21212,
+    user: 'h5eventnet',
+    password: 'jGpTCx6WaPW3KZjw',
+    secure: true,
+    secureOptions: {
+      rejectUnauthorized: false,
+    },
+  })
+  await client.ensureDir('/t/5')
+
+  console.log(await client.list(), '2.')
+
+  await client.clearWorkingDir()
+
+  await client.uploadFromDir('dist')
+
+  exit()
+}
+
+clientAndUpload()
