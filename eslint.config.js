62ab63a53acb51988a7b6771274a0dd677a90192
yulin <aaayulin@gmail.com>
2024-10-17 16:31:05 +0800

    chore: 更新eslint配置

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,9 @@
 import { includeIgnoreFile } from '@eslint/compat'
-import pluginJs from '@eslint/js'
+import skipFormatting from '@vue/eslint-config-prettier/skip-formatting'
+import vueTsEslintConfig from '@vue/eslint-config-typescript'
 import pluginVue from 'eslint-plugin-vue'
-import globals from 'globals'
 import path from 'node:path'
 import { fileURLToPath } from 'node:url'
-import tseslint from 'typescript-eslint'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
@@ -14,56 +13,36 @@ export default [
   includeIgnoreFile(gitignorePath),
 
   {
-    ignores: ['**/*skip*.js'],
+    name: 'app/files-to-lint',
+    files: ['**/*.{ts,mts,tsx,vue}'],
   },
 
   {
-    languageOptions: {
-      globals: {
-        ...globals.browser,
-      },
-    },
+    name: 'app/files-to-ignore',
+    ignores: ['**/dist/**', '**/dist-ssr/**', '**/coverage/**'],
   },
 
-  pluginJs.configs.recommended,
+  ...pluginVue.configs['flat/essential'],
+  ...vueTsEslintConfig(),
+  skipFormatting,
+
   {
-    name: 'rules/js',
+    files: ['**/*.{ts,js,cjs,mts,jsx,tsx,vue}'],
     rules: {
       'no-debugger': 0,
       'no-empty': 1,
       'no-prototype-builtins': 0,
       'no-undef': 0,
       'no-unused-vars': 0,
-    },
-  },
 
-  ...tseslint.configs.recommended,
-  {
-    name: 'rules/ts',
-    rules: {
       '@typescript-eslint/no-empty-function': 0,
       '@typescript-eslint/no-empty-object-type': 0,
       '@typescript-eslint/no-explicit-any': 0,
       '@typescript-eslint/no-unused-expressions': 0,
       '@typescript-eslint/no-unused-vars': 0,
       'prefer-rest-params': 0,
-    },
-  },
 
-  ...pluginVue.configs['flat/essential'],
-  { files: ['**/*.vue'], languageOptions: { parserOptions: { parser: tseslint.parser } } },
-  {
-    name: 'rules/vue',
-    rules: {
       'vue/multi-word-component-names': 0,
     },
   },
-
-  // prettier,
-  // {
-  //   name: 'rules/prettier',
-  //   rules: {
-  //     'prettier/prettier': 0,
-  //   },
-  // },
 ]
