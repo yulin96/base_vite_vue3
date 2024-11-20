6746e0204b6cee829f111674cbecbe5949f27a40
yulin <aaayulin@gmail.com>
2024-11-20 17:34:01 +0800

    feat: 添加可点击的二维码展示组件，支持放大缩小功能

---
+++ src/components/common/code.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+defineProps<{ code: string }>()
+
+const isBig = ref(false)
+const toggleCode = (dispatch?: boolean) => {
+  gsap.to('[code]', {
+    scale: dispatch ? 1 : isBig.value ? 1 : 2.2,
+    duration: 0.3,
+    ease: 'back',
+  })
+  isBig.value = dispatch || !isBig.value
+}
+</script>
+
+<template>
+  <div code @click="toggleCode()" class="relative z-20" v-bind="$attrs">
+    <img class="h-full w-full" :src="code" />
+  </div>
+  <VanOverlay :show="isBig" @click="toggleCode(false)" class="z-10"></VanOverlay>
+</template>
