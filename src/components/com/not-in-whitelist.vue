626c88d942cc1a214c9ece502207dd8b76263f9b
yulin <aaayulin@gmail.com>
2025-01-17 15:18:05 +0800

    feat: 替换提示组件中的图片为文本提示

--- src/components/com/not-in-whitelist.vue
+++ src/components/com/not-in-whitelist.vue
@@ -14,10 +14,9 @@ defineProps<{ title?: string }>()
           class="absolute -top-[100px] w-203"
           src="https://oss.eventnet.cn/H5/zz/public/tips/whitelist/icon.png"
         />
-        <img
-          class="mt-20 w-196"
-          src="https://oss.eventnet.cn/H5/zz/public/tips/whitelist/tips.png"
-        />
+        <div class="-mt-60 indent-4 text-50 font-semibold tracking-[4px] text-[#c42420]">
+          温馨提示
+        </div>
         <div
           class="mt-40 w-full whitespace-pre-line px-20 text-center indent-1 text-30 leading-[1.7] tracking-[1px]"
         >
