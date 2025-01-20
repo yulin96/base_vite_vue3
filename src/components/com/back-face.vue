e656d901e2097abc90cfa795be85d3ef3be28fd4
yulin <aaayulin@gmail.com>
2025-01-20 16:13:38 +0800

    feat: 添加 motion 库并优化 back-face 组件的翻转动画

--- src/components/com/back-face.vue
+++ src/components/com/back-face.vue
@@ -1,17 +1,13 @@
 <script setup lang="ts">
-import gsap from 'gsap'
+import { animate } from 'motion'
 import { useTemplateRef, watchPostEffect } from 'vue'
 
-const { flip } = defineProps<{ flip?: 'front' | 'back' }>()
+const { flip = false } = defineProps<{ flip?: boolean }>()
 
 const flipRef = useTemplateRef('flipRef')
 
 watchPostEffect(() => {
-  if (flip === 'front') {
-    gsap.to(flipRef.value!, { rotateY: 0, ease: 'power2', duration: 0.5 })
-  } else if (flip === 'back') {
-    gsap.to(flipRef.value!, { rotateY: 180, ease: 'power2', duration: 0.5 })
-  }
+  animate(flipRef.value!, { rotateY: flip ? 180 : 0 }, { type: 'tween', ease: 'easeOut' })
 })
 </script>
 
@@ -19,13 +15,13 @@ watchPostEffect(() => {
   <div class="perspective-[1200]">
     <section ref="flipRef" class="relative h-full w-full transform-3d">
       <div
-        :class="flip === 'front' ? 'pointer-events-auto' : 'pointer-events-none'"
+        :class="!flip ? 'pointer-events-auto' : 'pointer-events-none'"
         class="absolute left-0 top-0 h-full w-full backface-hidden"
       >
         <slot></slot>
       </div>
       <div
-        :class="flip === 'back' ? 'pointer-events-auto' : 'pointer-events-none'"
+        :class="flip ? 'pointer-events-auto' : 'pointer-events-none'"
         class="absolute left-0 top-0 h-full w-full backface-hidden rotate-y-[180]"
       >
         <slot name="back"> </slot>
