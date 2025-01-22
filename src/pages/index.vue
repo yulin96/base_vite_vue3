c80df4241470b7526dae50814795c84f1f576a7a
yulin <aaayulin@gmail.com>
2025-01-22 14:58:56 +0800

    feat: 在 index.vue 中添加滚动动画效果，优化用户体验

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,13 +1,57 @@
 <script setup lang="ts">
+import { animate, scroll } from 'motion'
 import { onMounted } from 'vue'
 
 /*  */
-onMounted(() => {})
+onMounted(() => {
+  scroll(animate('.box10', { x: [100, 0], opacity: [0.1, 1] }), {
+    container: document.querySelector('.index') as HTMLElement,
+    offset: [0, 0.4],
+  })
+
+  scroll(
+    animate('.move_box', {
+      transform: ['none', `translateX(-500vw)`],
+    }),
+    {
+      target: document.querySelector('.move_b2') as HTMLElement,
+      container: document.querySelector('.index') as HTMLElement,
+    },
+  )
+})
 </script>
 
 <template>
   <section class="index">
-    <main class="content"></main>
+    <main class="content flex flex-col items-center">
+      <div
+        v-for="item in 10"
+        :key="item"
+        :class="`box${item}`"
+        class="center my-30 size-200 rounded-12 bg-sky-500 text-white"
+      >
+        {{ item }}
+      </div>
+      <div class="move_b2 h-[500vh] w-full">
+        <div class="move_box sticky top-0 flex h-[100vh] w-full">
+          <div
+            v-for="item in 6"
+            :key="item"
+            class="center h-[100vh] w-full flex-[0_0_auto] border bg-red-200"
+          >
+            {{ item }}
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eum earum eveniet nisi aliquam
+            est distinctio adipisci, ut unde voluptatum natus sed culpa dolorem similique rem, vitae
+            cum. Minima, provident molestias?
+          </div>
+        </div>
+      </div>
+      <div class="">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. In dicta maxime, at error facilis
+        exercitationem deleniti animi autem cumque nesciunt a itaque sint praesentium similique
+        labore vel, consequuntur quos dolore?
+      </div>
+    </main>
   </section>
 </template>
 
