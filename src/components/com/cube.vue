acd3f63ff19c356c96d3410eac515df39c009fdb
yulin <aaayulin@gmail.com>
2025-01-23 17:06:32 +0800

    feat: 添加旋转立方体动画组件，更新样式和关键帧定义

---
+++ src/components/com/cube.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+const { play = false } = defineProps<{ play?: boolean }>()
+</script>
+
+<template>
+  <div class="mx-auto mt-90 size-200 perspective-distant">
+    <div
+      class="relative size-200 transform-3d *:absolute *:size-200 *:bg-cover"
+      :class="[play ? 'animate-rotate-cube' : '']"
+    >
+      <div class="transform-[translateZ(100px)]">
+        <slot></slot>
+      </div>
+      <div class="transform-[rotateY(180deg)_translateZ(100px)]">
+        <slot></slot>
+      </div>
+      <div class="transform-[rotateY(-90deg)_translateZ(100px)]">
+        <slot></slot>
+      </div>
+      <div class="transform-[rotateY(90deg)_translateZ(100px)]">
+        <slot></slot>
+      </div>
+      <div class="transform-[rotateX(90deg)_translateZ(100px)]">
+        <slot></slot>
+      </div>
+      <div class="transform-[rotateX(-90deg)_translateZ(100px)]">
+        <slot></slot>
+      </div>
+    </div>
+  </div>
+</template>
