d15e9074f68f7bde6021057047dfce9606d4fee7
yulin <aaayulin@gmail.com>
2025-02-18 14:51:28 +0800

    feat: 更新 barrage 组件，添加随机颜色和图钉功能，优化动画效果

--- src/components/com/barrage.vue
+++ src/components/com/barrage.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
+import { random, sample } from 'es-toolkit'
 import { animate } from 'motion'
-import { onMounted } from 'vue'
+import { onMounted, useTemplateRef } from 'vue'
+import pinSvg from '~/assets/imgs/pin.svg'
 import { randomString } from '~/utils/common'
 
 const uuid = randomString()
@@ -8,16 +10,17 @@ const uuid2 = randomString()
 
 const {
   gap = 20,
-  speed = 50,
+  speed = 90,
   barrageList,
 } = defineProps<{ barrageList: Array<any>; gap?: number; speed?: number }>()
 
 const currentId = defineModel<number>({ required: true })
 
+const barrageBox = useTemplateRef('barrageBox')
 onMounted(() => {
   const box = document.querySelector(`[uuid="${uuid}"]`) as HTMLDivElement | null
   if (!box) return console.error('box is null')
-
+  clientWidth = barrageBox.value?.clientWidth || innerWidth
   for (const item of box.children) {
     item.classList.add(uuid2)
   }
@@ -31,6 +34,7 @@ onMounted(() => {
   autoCreateBarrage()
 })
 
+let clientWidth = innerWidth
 async function createBarrage({
   params,
   gap,
@@ -44,11 +48,13 @@ async function createBarrage({
 }) {
   if (!params) return
 
-  const index = Array.from(parents).findIndex(
-    (item) =>
+  const index = Array.from(parents).findIndex((item) => {
+    return (
       item.lastChild === null ||
-      (item.lastChild as HTMLDivElement).getBoundingClientRect().right < innerWidth,
-  )
+      (item.lastChild as HTMLDivElement).getBoundingClientRect().right <
+        (barrageBox.value?.getBoundingClientRect().right || clientWidth)
+    )
+  })
 
   if (index === -1) return
 
@@ -56,17 +62,38 @@ async function createBarrage({
 
   const barrage = document.createElement('div')
   barrage.classList.add('barrage')
-  barrage.style.left = `${innerWidth + gap}px`
+  barrage.classList.add(sample(['left', 'right']))
+  gap = random(12, 50)
+  barrage.style.left = `${clientWidth + gap}px`
   barrage.innerHTML = `
-    <img src="${params.img}" alt="${params.name}"/>
-    <span>${params.name}：</span>
     <span>${params.content}</span>
+    ${params.pin ? `<img class="pin_my" src="${pinSvg}" />` : ''}
   `
+
+  const colors = [
+    { background: '#FC6760', foreground: '#fff' },
+    { background: '#5DEC6D', foreground: '#fff' },
+    { background: '#57A8EA', foreground: '#fff' },
+    { background: '#4C94FD', foreground: '#fff' },
+    { background: '#E88823', foreground: '#fff' },
+    { background: '#FFFF48', foreground: '#333' },
+  ]
+  const { background, foreground } = params.pin
+    ? { background: '#493D9E', foreground: '#fff' }
+    : sample(colors)
+  barrage.style.backgroundColor = background
+  barrage.style.color = foreground
+  if (params.pin) {
+    barrage.style.fontWeight = 'bold'
+  }
+  barrage.style.setProperty('--data-color', background)
+
+  params.pin = false
 
   const parent = parents[index]
   parent.appendChild(barrage)
 
-  const offset = Math.floor(innerWidth + barrage.clientWidth + gap)
+  const offset = Math.floor(clientWidth + barrage.clientWidth + gap)
   const _animate = animate(barrage, { x: -offset }, { duration: offset / speed, ease: 'linear' })
   await _animate
   _animate.stop()
@@ -84,7 +111,7 @@ async function createBarrage({
 </script>
 
 <template>
-  <div v-bind="{ uuid }" class="barrage-box w-full">
+  <div v-bind="{ uuid }" ref="barrageBox" class="barrage-box w-full">
     <slot>
       <div></div>
     </slot>
@@ -95,13 +122,44 @@ async function createBarrage({
 .barrage {
   position: absolute;
   border-radius: 50px;
-  background-color: #eaf7ff;
-  border: 2px solid #333;
+  /* background-color: #eaf7ff; */
+  /* border: 2px solid #333; */
   width: max-content;
-  padding: 12px 26px;
-  font-size: 26px;
+  /* padding: 12px 26px; */
+  padding: 4px 26px;
+  font-size: 24px;
   display: flex;
   align-items: center;
+}
+
+.barrage .pin_my {
+  width: 40px;
+  height: 40px;
+  position: absolute;
+  top: -12px;
+  right: -12px;
+}
+
+.barrage.left::after {
+  content: '';
+  width: 0;
+  height: 0;
+  position: absolute;
+  top: 100%;
+  left: 20px;
+  border-width: 8px 12px;
+  border-color: var(--data-color) transparent transparent var(--data-color);
+}
+
+.barrage.right::after {
+  content: '';
+  width: 0;
+  height: 0;
+  position: absolute;
+  top: 100%;
+  right: 20px;
+  border-width: 8px 12px;
+  border-color: var(--data-color) var(--data-color) transparent transparent;
 }
 
 .barrage img:nth-child(1) {
