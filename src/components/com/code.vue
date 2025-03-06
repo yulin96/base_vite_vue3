87006ce52f899ded30ca74313c66ed6b9335d49d
yulin <aaayulin@gmail.com>
2025-03-06 16:11:22 +0800

    feat: 优化代码缩放动画，调整目标位置计算逻辑

--- src/components/com/code.vue
+++ src/components/com/code.vue
@@ -1,29 +1,49 @@
 <script setup lang="ts">
 import { animate } from 'motion'
 import { v4 } from 'uuid'
-import { ref } from 'vue'
+import { onMounted, ref } from 'vue'
 
 defineProps<{ code: string }>()
 
 const uuid = v4()
 
 const isBig = ref(false)
-const toggleCode = (dispatch?: boolean) => {
+
+const target: [number, number, number] = [4, 120, 0]
+const toggleCode = () => {
   animate(
     `#code-${uuid}`,
-    { scale: dispatch ? 1 : isBig.value ? 1 : 2.2 },
+    {
+      scale: isBig.value ? 1 : target[0],
+      y: isBig.value ? 0 : target[1],
+      x: isBig.value ? 0 : target[2],
+    },
     { type: 'tween', ease: 'backOut' },
   )
 
-  isBig.value = dispatch || !isBig.value
+  isBig.value = !isBig.value
 }
 
 defineExpose({ toggleCode })
+
+onMounted(() => {
+  const bounding = document.querySelector(`#code-${uuid}`)?.getBoundingClientRect()
+  const app = document.querySelector('#app')
+  if (bounding && app) {
+    const { width, height, top, left } = bounding
+
+    target[0] = Math.floor((app.clientWidth - 50) / width)
+    target[1] = Math.floor(
+      app.clientHeight / 2 - (top + height / 2) + (top < app.clientHeight / 2 ? -20 : 20),
+    )
+    target[2] = Math.floor(innerWidth / 2 - (left + width / 2))
+  }
+})
 </script>
 
 <template>
-  <div :id="`code-${uuid}`" class="relative z-[20]" v-bind="$attrs" @click="toggleCode()">
+  <div :id="`code-${uuid}`" class="relative z-[20]" v-bind="$attrs" @click="toggleCode">
     <img class="h-full w-full" :src="code" />
   </div>
-  <van-overlay :show="isBig" class="z-[10]" @click="toggleCode(false)"></van-overlay>
+  <van-overlay :show="isBig" class="z-[10]" @click="toggleCode"></van-overlay>
 </template>
