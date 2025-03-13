b8774738aed210fd316f777ff4f4a22802f4da68
yulin <aaayulin@gmail.com>
2025-03-13 14:37:16 +0800

    feat: 添加 NumberFlow 组件，支持动画效果

---
+++ src/components/com/number-flow.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import NumberFLow from '@number-flow/vue'
+import gsap from 'gsap'
+import { v4 } from 'uuid'
+import { onMounted } from 'vue'
+
+const props = defineProps<{ value: number; animate?: boolean }>()
+
+const id = v4()
+let dom: HTMLElement | null = null
+let color = 'null'
+
+const handleAnimationsStart = () => {
+  if (dom && props.animate) gsap.to(dom, { color: '#f00' })
+}
+
+const handleAnimationsFinish = () => {
+  if (dom && props.animate) gsap.to(dom, { color: color })
+}
+
+onMounted(() => {
+  dom = document.getElementById(id)
+  if (dom) color = getComputedStyle(dom).color
+})
+</script>
+
+<template>
+  <NumberFLow
+    :id="id"
+    :value="value"
+    will-change
+    @animationsstart="handleAnimationsStart"
+    @animationsfinish="handleAnimationsFinish"
+  ></NumberFLow>
+</template>
