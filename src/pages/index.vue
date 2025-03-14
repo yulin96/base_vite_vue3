6a101e25e6fb514ea630dc072624df5bbd0a5268
yulin <aaayulin@gmail.com>
2025-03-14 16:05:03 +0800

    feat: 在首页添加登录按钮并实现点击事件

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -3,6 +3,8 @@ import { useToggle } from '@vueuse/core'
 import gsap from 'gsap'
 import { onMounted } from 'vue'
 
+const login = () => {}
+
 const [isLoaded, setIsLoaded] = useToggle(false)
 
 onMounted(() => {
@@ -14,7 +16,9 @@ onMounted(() => {
 
 <template>
   <section class="index group" :aria-expanded="isLoaded">
-    <main class="content"></main>
+    <main class="content flex flex-col items-center space-y-12 pt-50">
+      <div class="center h-90 w-full border" @click="login">登录</div>
+    </main>
   </section>
 </template>
 
