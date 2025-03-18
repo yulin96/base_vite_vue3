676336ee2d1e7d774cc841e6f59d7d77a8af2cd4
yulin <aaayulin@gmail.com>
2025-03-18 15:50:42 +0800

    feat: 添加 border-button 组件，支持动态背景色和旋转效果

---
+++ src/components/com/border-button.vue
@@ -0,0 +1,56 @@
+<script setup lang="ts">
+defineProps<{ color: string }>()
+</script>
+
+<template>
+  <div
+    class="center rounded-full p-0 indent-2 font-semibold tracking-2"
+    :style="{ backgroundColor: color }"
+  >
+    <div class="conic center h-full w-full rounded-full">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+@keyframes rotate {
+  100% {
+    transform: rotate(1turn);
+  }
+}
+
+.conic {
+  overflow: hidden;
+  position: relative;
+  z-index: 20;
+}
+.conic::before {
+  content: '';
+  position: absolute;
+  z-index: -2;
+  left: -50%;
+  top: -50%;
+  width: 200%;
+  height: 200%;
+  background-repeat: no-repeat;
+  background-position: 0 0;
+  background-image: conic-gradient(
+    transparent 0deg,
+    transparent 50deg,
+    #191919 110deg,
+    transparent 130deg,
+    transparent 360deg
+  );
+  animation: rotate 4s linear infinite;
+}
+
+.conic::after {
+  content: '';
+  position: absolute;
+  z-index: -1;
+  background-color: #e4dfcb;
+  border-radius: 9999px;
+  inset: 4px;
+}
+</style>
