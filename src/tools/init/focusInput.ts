c7a0b3dca09daa3e0b1ec8d3d6b9b1ac4cfb7edc
yulin <aaayulin@gmail.com>
2025-03-19 14:40:02 +0800

    feat: 添加 focusInput 函数，使用 GSAP 实现输入框聚焦动画效果

---
+++ src/tools/init/focusInput.ts
@@ -0,0 +1,14 @@
+import gsap from 'gsap'
+
+export function focusInput(name: string) {
+  const dom = document.querySelector(`[${name}]`)
+  if (!dom) return true
+  // dom.scrollIntoView({ behavior: 'smooth', block: 'center', inline: 'center' })
+  gsap.timeline().to(dom, { duration: 0.15, scale: 1.1 }).to(dom, { duration: 0.15, scale: 1 })
+  gsap.to(dom, { duration: 0.3, border: '2px dashed #424242' }).then(() => {
+    setTimeout(() => {
+      gsap.to(dom, { duration: 0.3, border: '2px dashed #42424200', delay: 0.3 })
+    }, 600)
+  })
+  return true
+}
