1cd0266f53ba02ba786c3abbc2e1db8189cb37a5
yulin <aaayulin@gmail.com>
2025-03-21 14:40:17 +0800

    feat: 添加 apiMenus 函数

---
+++ src/api/index.ts
@@ -0,0 +1,20 @@
+import { useRequest } from '~/hooks/useRequest'
+
+const { get: getMenus } = useRequest()
+export const apiMenus = (title: string) => {
+  type T = {
+    title: string
+    status: number
+    url: string
+    remark: string
+  }
+  return new Promise<[boolean, T | null]>((resolve) => {
+    getMenus('https://cdeapi.event1.cn/api/cmenu', { title })
+      .then((res: any) => {
+        resolve([res.data?.status == 1, res.data])
+      })
+      .catch(() => {
+        resolve([false, null])
+      })
+  })
+}
