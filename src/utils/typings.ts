aae327024037c9a97d186ff3e7e461368e2346e1
yulin <aaayulin@gmail.com>
2025-04-05 18:56:50 +0800

    feat: 增强颜色处理功能，添加颜色缓存机制，优化颜色转换逻辑

--- src/utils/typings.ts
+++ src/utils/typings.ts
@@ -1,3 +1,22 @@
 import type { RouteNamedMap } from 'vue-router/auto-routes'
 
 export type linkMapType = { [key in keyof RouteNamedMap]?: keyof RouteNamedMap }
+
+/**
+ * RGB 颜色值类型
+ * 表示为包含三个元素的数组，分别对应红、绿、蓝三个颜色通道的值（0-255）
+ */
+export type RGBColor = [number, number, number]
+
+/**
+ * RGBA 颜色值类型
+ * 表示为包含四个元素的数组，分别对应红、绿、蓝、透明度四个通道的值
+ * RGB 通道范围为 0-255，透明度通道范围为 0-1
+ */
+export type RGBAColor = [number, number, number, number]
+
+/**
+ * 16进制颜色值类型
+ * 格式为 #RRGGBB 或 #RGB
+ */
+export type HexColor = string
