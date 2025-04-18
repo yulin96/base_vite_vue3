30a12e55001cd91e8e966ff35844e6ebb889beab
yulin <aaayulin@gmail.com>
2025-04-18 15:42:02 +0800

    chore: 优化 index.vue，调整 avatarModel 资源定义，简化相机结构

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -48,24 +48,25 @@ onMounted(() => {
       device-orientation-permission-ui="enabled: false"
     >
       <a-entity mindar-camera>
-        <a-camera look-controls="enabled: false">
-          <a-entity
-            gltf-model="url(https://oss.eventnet.cn/H5/zz/auto2/a/8/xhs6.glb)"
-            scale="0.5 0.5 0.5"
-            position="0 -0.5 -1"
-          ></a-entity>
-        </a-camera>
+        <a-camera look-controls="enabled: false"> </a-camera>
       </a-entity>
 
+      <a-assets>
+        <a-asset-item
+          id="avatarModel"
+          src="https://oss.eventnet.cn/H5/zz/auto2/a/8/xhs6.glb"
+        ></a-asset-item>
+      </a-assets>
+
       <a-entity class="entity entity1" mindar-image-target="targetIndex: 0" data-card="1号">
-        <!-- <a-gltf-model
+        <a-gltf-model
           rotation="0 0 0 "
           position="0 -0.5 0.1"
           scale="0.2 0.2 0.2"
           src="#avatarModel"
           gesture-handler="minScale: 0.1; maxScale: 2"
         >
-        </a-gltf-model> -->
+        </a-gltf-model>
         <!--   animation="property: position; to: 0 0.1 0.1; dur: 1000; easing: easeInOutQuad; loop: true; dir: alternate" -->
       </a-entity>
     </a-scene>
