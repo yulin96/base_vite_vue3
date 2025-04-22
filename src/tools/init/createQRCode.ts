3c7f5b814fb656c2f217efb1d726eff855311aa8
yulin <aaayulin@gmail.com>
2025-04-22 15:51:09 +0800

    fix: 修复 createQRCode 函数中的 URL 处理逻辑，确保去除哈希部分

--- src/tools/init/createQRCode.ts
+++ src/tools/init/createQRCode.ts
@@ -6,7 +6,7 @@ export function createQRCode(app: HTMLDivElement) {
     document.body.removeChild(document.querySelector('.code-tips.pc')!)
 
   const pageURL = location.href.replace('pc.html', 'index.html')
-  const clearedUrl = removeUrlParams(pageURL, 't')
+  const clearedUrl = removeUrlParams(pageURL, 't').split('#')[0]
 
   QRCode.toDataURL(clearedUrl, { margin: 2, errorCorrectionLevel: 'H', width: 900 }).then((res) => {
     const left = Math.round(app.getBoundingClientRect().right + innerWidth / 100)
