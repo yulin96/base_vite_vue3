14b79df407db004658d81154174059fba4e79d65
yulin <aaayulin@gmail.com>
2025-04-27 14:59:31 +0800

    feat: 将路由导航辅助函数和防抖函数移至 utils.ts，简化代码结构

--- src/tools/utils.ts
+++ src/tools/utils.ts
@@ -1,8 +1,19 @@
+import router from '@/router'
 import { type ClassValue, clsx } from 'clsx'
+import { debounce } from 'es-toolkit'
 import { twMerge } from 'tailwind-merge'
+import type { RouteLocationRaw } from 'vue-router'
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
 export type ObjectValues<T> = T[keyof T]
+
+export function debounceLeading<F extends (...args: any[]) => void>(fn: F, time = 600) {
+  return debounce(fn, time, { edges: ['leading'] })
+}
+
+export function routerTo(to: RouteLocationRaw) {
+  return router.replace(to)
+}
