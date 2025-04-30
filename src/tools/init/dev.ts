34378cba9c399b5b7011e136b48c36308bd4afc7
yulin <aaayulin@gmail.com>
2025-04-30 11:10:01 +0800

    refactor: 优化 vConsole 脚本加载方式

--- src/tools/init/dev.ts
+++ src/tools/init/dev.ts
@@ -1,11 +1,10 @@
+import { loadScript } from '@/utils/dom'
+
 const params = new URLSearchParams(window.location.search)
 
 if (params.get('dev') !== null) {
-  setTimeout(() => {
-    const vConsoleScript = document.createElement('script')
-    vConsoleScript.src = 'https://oss.eventnet.cn/H5/zz/public/vConsole.js'
-    document.body.appendChild(vConsoleScript)
+  loadScript('https://oss.eventnet.cn/H5/zz/public/vConsole.js').then(() => {
     //@ts-expect-error window.VConsole
-    vConsoleScript.onload = () => new window.VConsole()
-  }, 0)
+    new window.VConsole()
+  })
 }
