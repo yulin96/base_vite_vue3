56b63bd1c1b9fd6b120f664699f9429cb03042a0
yulin <aaayulin@gmail.com>
2025-05-30 14:44:08 +0800

    wip

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { useState } from '@/hooks/useState'
-import gsap from '@/shared/gsap'
+import { gsap } from '@/shared'
 import { onMounted } from 'vue'
 
 const [loaded, setLoaded] = useState(false)
