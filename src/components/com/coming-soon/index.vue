fb8c102b2703fca3c7a91a15133d5a187856c7fd
yulin <aaayulin@gmail.com>
2025-06-11 15:36:05 +0800

    feat: 添加即将到来的弹窗组件及相关功能

--- src/components/com/coming-soon/index.vue
+++ src/components/com/coming-soon/index.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
 import { ref } from 'vue'
-import bg from './bg.png'
 
 const props = defineProps<{
   text?: string
@@ -28,15 +27,12 @@ defineExpose({
     transition="popup"
     @closed="props?.onClose?.()"
   >
-    <div
-      class="back-full flex h-420 w-520 flex-col items-center pt-40 shadow-2xl"
-      :style="{ backgroundImage: `url(${bg})` }"
-    >
-      <img class="h-155" src="./icon.png" alt="" />
-      <div class="text-32 tracking-2 mt-20 font-semibold">{{ text || '报名成功' }}</div>
+    <div class="back-full rounded-30 flex h-520 w-520 flex-col items-center bg-white pt-40 shadow-2xl">
+      <img class="h-260" src="./icon.png" alt="" />
+      <div class="text-32 tracking-2 mt-12 font-semibold">{{ text || '敬请期待...' }}</div>
       <div
         btn
-        class="center tracking-2 mt-36 h-80 w-330 cursor-pointer rounded-full bg-linear-180 from-[#FE4600] to-[#FE4600] indent-2 text-white"
+        class="center tracking-2 mt-36 h-80 w-330 cursor-pointer rounded-full bg-linear-180 from-[#000] to-[#000] indent-2 text-white"
         @click="handleClose"
       >
         好的
--- src/shared/popup/showComingSoon.ts
+++ src/shared/popup/showComingSoon.ts
@@ -1,10 +1,10 @@
-import regComplete from '@/components/com/reg-complete/index.vue'
+import comingSoon from '@/components/com/coming-soon/index.vue'
 import { createApp } from 'vue'
 
 let appRoot: HTMLElement | null = null
 let app: ReturnType<typeof createApp> | null = null
 
-export function showRegComplete(text?: string) {
+export function showComingSoon(text?: string) {
   if (app) {
     app._instance?.exposed?.show()
     return
@@ -13,7 +13,7 @@ export function showRegComplete(text?: string) {
   appRoot = document.querySelector('#app')
   if (!appRoot) return console.error('App root not found')
   appRoot.appendChild(container)
-  app = createApp(regComplete, { text })
+  app = createApp(comingSoon, { text })
 
   app.mount(container)
 }
